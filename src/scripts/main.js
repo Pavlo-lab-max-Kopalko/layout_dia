1f27b0e5f6bd59d71f1cd024932342c82e79fdd9
Pavlo <oksanakopalko@gmail.com>
2025-02-19 19:38:27 +0200

    щось_напиши

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const arrowLeft = document.getElementsByClassName('header__left')[0];
+const arrowRight = document.getElementsByClassName('header__right')[0];
+const headerBackgraund
+  = document.getElementsByClassName('header__background')[0];
+
+console.log(arrowLeft);
+console.log(arrowRight);
+console.log(headerBackgraund);
+
+const photos = [
+  '/images/slider-photo1.png',
+  '/images/slider-photo2.png',
+  '/images/slider-photo3.png',
+  '/images/background-photo.png',
+];
+
+console.log(headerBackgraund.style.backgroundImage);
+
+let index = 0;
+
+arrowRight.addEventListener('click', () => {
+  index += 1;
+  headerBackgraund.style.backgroundImage = `url${photos[index]}`;
+});
